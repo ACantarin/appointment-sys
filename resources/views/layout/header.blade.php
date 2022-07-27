3bb407154564f402f4b7d8301467d5642761a405
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 16:44:17 -0300

    Remove os scripts do header do sistema

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,47 +4,25 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-    <script type="text/javascript "src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script type="text/javascript">
-      $(document).ready(function(){
-        M.updateTextFields();
-        $('.sidenav').sidenav();
-        $('select').formSelect();
-      });
-    </script>
-
-    <script>
-        var message = "{{Session::get('alert')}}";
-        if (message) {
-            alert(message);
-        }
-    </script>
-
   </head>
-
   <body>
-<header>
-
-  <nav>
-    <div class="nav-wrapper blue">
-      <a href="#" class="brand-logo">Controle de Consultas</a>
-      <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="{{ route('appointments.index') }}">Consultas</a></li>
-        <li><a href="{{ route('patients.index') }}">Pacientes</a></li>
-        <li><a href="{{ route('doctors.index') }}">Médicos</a></li>
-        <li><a href="{{ route('specialties.index') }}">Especialidades</a></li>
-      </ul>
-    </div>
-  </nav>
-
-  <ul class="sidenav" id="mobile">
-    <li><a href="#">Consultas</a></li>
-    <li><a href="{{ route('patients.index') }}"">Pacientes</a></li>
-    <li><a href="{{ route('doctors.index') }}"">Médicos</a></li>
-    <li><a href="{{ route('specialties.index') }}"">Especialidades</a></li>
-  </ul>
-
-</header>
+  <header>
+    <nav>
+      <div class="nav-wrapper blue">
+        <a href="#" class="brand-logo">Controle de Consultas</a>
+        <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="{{ route('appointments.index') }}">Consultas</a></li>
+          <li><a href="{{ route('patients.index') }}">Pacientes</a></li>
+          <li><a href="{{ route('doctors.index') }}">Médicos</a></li>
+          <li><a href="{{ route('specialties.index') }}">Especialidades</a></li>
+        </ul>
+      </div>
+    </nav>
+    <ul class="sidenav" id="mobile">
+      <li><a href="#">Consultas</a></li>
+      <li><a href="{{ route('patients.index') }}"">Pacientes</a></li>
+      <li><a href="{{ route('doctors.index') }}"">Médicos</a></li>
+      <li><a href="{{ route('specialties.index') }}"">Especialidades</a></li>
+    </ul>
+  </header>
