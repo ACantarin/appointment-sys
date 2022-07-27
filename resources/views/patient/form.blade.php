38a076eb7f67eb40d257e9ee4069e176f7aab7ed
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 18:52:15 -0300

    Inclui IDs nos inputs do formulário do paciente

--- resources/views/patient/form.blade.php
+++ resources/views/patient/form.blade.php
@@ -4,31 +4,31 @@
 </div>
 
 <div class="input-field">
-  <input type="text" name="document" value="{{ isset($patient->document) ? $patient->document : '' }}">
+  <input type="text" name="document" id="js-patient-document" value="{{ isset($patient->document) ? $patient->document : '' }}">
   <label>CPF</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="age" value="{{ isset($patient->age) ? $patient->age : '' }}">
+  <input type="text" name="age" id="js-patient-age" value="{{ isset($patient->age) ? $patient->age : '' }}">
   <label>Idade</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="email" value="{{ isset($patient->email) ? $patient->email : '' }}">
+  <input type="text" name="email" id="js-patient-email" value="{{ isset($patient->email) ? $patient->email : '' }}">
   <label>E-mail</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="zip_code" value="{{ isset($patient->zip_code) ? $patient->zip_code : '' }}">
+  <input type="text" name="zip_code" id="js-patient-zip-code" value="{{ isset($patient->zip_code) ? $patient->zip_code : '' }}">
   <label>CEP</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="address" value="{{ isset($patient->address) ? $patient->address : '' }}">
+  <input type="text" name="address" id="js-patient-address" value="{{ isset($patient->address) ? $patient->address : '' }}">
   <label>Endereço</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="address_number" value="{{ isset($patient->address_number) ? $patient->address_number : '' }}">
+  <input type="text" name="address_number" id="js-patient-address-number" value="{{ isset($patient->address_number) ? $patient->address_number : '' }}">
   <label>Nº</label>
 </div>
