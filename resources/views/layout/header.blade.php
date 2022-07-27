851a9512de68af285b7ecd5c27ea38342a153fba
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 14:32:49 -0300

    Move os scripts JS do footer para o header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,6 +5,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
+    <script type="text/javascript "src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        M.updateTextFields();
+        $('.sidenav').sidenav();
+        $('select').formSelect();
+      });
+    </script>
+
     <script>
         var message = "{{Session::get('alert')}}";
         if (message) {
