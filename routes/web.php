079c5cc482561912e1d071eb64cdaddd7ebed521
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 11:26:51 -0300

    Retira rotas não utilizadas/modificadas

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/specialties", ["as" => "specialties.index", "uses" => "App\Http\Controllers\SpecialtyController@index"]);
-Route::post("/specialties/save", [SpecialtyController::class, "store"]);
 Route::get("/specialties/add",["as" => "specialties.add", "uses" => "App\Http\Controllers\SpecialtyController@add"]);
 Route::get("/specialties/save",["as" => "specialties.save", "uses" => "App\Http\Controllers\SpecialtyController@store"]);
 
@@ -25,9 +24,7 @@ Route::get("/doctors", ["as" => "doctors.index", "uses" => "App\Http\Controllers
 Route::get("/doctors/add",["as" => "doctors.add", "uses" => "App\Http\Controllers\DoctorController@add"]);
 Route::post("/doctors/save",["as" => "doctors.save", "uses" => "App\Http\Controllers\DoctorController@store"]);
 
-Route::get("/patients", [PatientController::class, "index"]);
 Route::get("/patients", ["as" => "patients.index", "uses" => "App\Http\Controllers\PatientController@index"]);
-Route::post("/patients/save", [PatientController::class, "store"]);
 
 Route::post("/test", function(Request $request) {
     dd($request);
