d4e151242f6cfff4f2ca604e01856f5f2e86a38e
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 16:43:48 -0300

    Retira tag script

--- resources/views/appointment/add.blade.php
+++ resources/views/appointment/add.blade.php
@@ -4,14 +4,13 @@
 
 @section("content")
     <div class="container">
-    <h3 class="center">Adicionar Consulta</h3>
-    <div class="row">
-        <form class="" action="{{ route('appointments.save') }}" method="post" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        @include("appointment.form")
-        <button class="btn deep-orange">Salvar</button>
-        </form>
-    </div>
-
+        <h3 class="center">Adicionar Consulta</h3>
+        <div class="row">
+            <form class="" action="{{ route('appointments.save') }}" method="post" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            @include("appointment.form")
+            <button class="btn deep-orange">Salvar</button>
+            </form>
+        </div>
     </div>
 @endsection
