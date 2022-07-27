ab0d29f9013a1782469a2af497f61d56160a7d98
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 18:52:59 -0300

    Faz o preenchiemento do endereço do paciente com base no CEP informado

--- resources/views/js/scripts.blade.php
+++ resources/views/js/scripts.blade.php
@@ -1,16 +1,35 @@
 <script type="text/javascript">
     $(document).ready(function(){
-    M.updateTextFields();
-    $('.sidenav').sidenav();
-    $('select').formSelect();
+        M.updateTextFields();
+        $('.sidenav').sidenav();
+        $('select').formSelect();
 
-    var message = "{{Session::get('alert')}}";
-    if (message) {
-        alert(message);
-    }
+        var message = "{{Session::get('alert')}}";
+        if (message) {
+            alert(message);
+        }
 
-    
+        var baseApiUrl = "http://127.0.0.1:8000/api";
 
+        $("#js-patient-zip-code").on("change", function () {
+            $.ajax({
+                url: baseApiUrl + "/address/" + this.value,
+                type: "GET",
+                dataType: "json",
+                success: function(response) {
+                    console.log(response.data);
+                    if (response.success == true) {
+                        $("#js-patient-address").val(response.data.logradouro);
+                    } else {
+                        alert(response.message);
+                        $("#js-patient-address").val("");
+                    }                   
+                },
+                fail: function () {
+                    alert("Erro ao buscar o endereço");
+                }
+            });
+        });
 
     });
 </script>
