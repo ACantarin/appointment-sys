5506e46853aa7ade4a6554a7ba485dbb74fa8ba9
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 11:14:17 -0300

    Inclui alert de mensagem no header do sistema

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,6 +4,14 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <script>
+        var message = "{{Session::get('alert')}}";
+        if (message) {
+            alert(message);
+        }
+    </script>
+
   </head>
 
   <body>
@@ -15,18 +23,18 @@
       <a href="#" data-target="mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="#">Consultas</a></li>
-        <li><a href="#">Pacientes</a></li>
-        <li><a href="#">Médicos</a></li>
-        <li><a href="#">Especialidades</a></li>
+        <li><a href="{{ route('patients.index') }}">Pacientes</a></li>
+        <li><a href="{{ route('doctors.index') }}">Médicos</a></li>
+        <li><a href="{{ route('specialties.index') }}">Especialidades</a></li>
       </ul>
     </div>
   </nav>
 
   <ul class="sidenav" id="mobile">
-    <li><a href="/">Consultas</a></li>
-    <li><a href="/">Pacientes</a></li>
-    <li><a href="/">Médicos</a></li>
-    <li><a href="/">Especialidades</a></li>
+    <li><a href="#">Consultas</a></li>
+    <li><a href="{{ route('patients.index') }}"">Pacientes</a></li>
+    <li><a href="{{ route('doctors.index') }}"">Médicos</a></li>
+    <li><a href="{{ route('specialties.index') }}"">Especialidades</a></li>
   </ul>
 
 </header>
