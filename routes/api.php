9eb4eac891a5ac908094bdb39c321a2aa0c8c901
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 18:51:50 -0300

    Inclui rota para busca de endereço por CEP

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,6 @@
 <?php
 
-use App\Http\Controllers\LegalResponsibleController;
-use App\Http\Controllers\PatientController;
 use App\Http\Controllers\AddressController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/address/{zip_code}", [AddressController::class, "show"]);
