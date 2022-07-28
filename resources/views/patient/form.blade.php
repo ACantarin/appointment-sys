172350970f47d622e39689534174e520b1a78b45
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 22:10:33 -0300

    Incluir atributo de campos obrigatórios

--- resources/views/patient/form.blade.php
+++ resources/views/patient/form.blade.php
@@ -1,46 +1,51 @@
 <div class="input-field">
-  <input type="text" name="name" value="{{ isset($patient->name) ? $patient->name : '' }}">
-  <label>Nome</label>
+  <input type="text" name="name" required>
+  <label>Nome *</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="document" id="js-patient-document" value="{{ isset($patient->document) ? $patient->document : '' }}">
-  <label>CPF</label>
+  <input type="text" name="document" id="js-patient-document" required>
+  <label>CPF *</label>
 </div>
 
 <div class="input-field">
-  <input type="number" min="0" max="120" name="age" id="js-patient-age" value="{{ isset($patient->age) ? $patient->age : '' }}">
-  <label>Idade</label>
+  <input type="number" min="0" max="120" name="age" id="js-patient-age" required>
+  <label>Idade *</label>
 </div>
 
 <div id="js-legal-responsible-data" hidden>
   <div class="input-field">
-    <input type="text" name="legal-responsible-name" id="js-legal-responsible-name">
-    <label>Responsável</label>
+    <input type="text" name="legal-responsible-name" id="js-legal-responsible-name" required>
+    <label>Responsável *</label>
   </div>
 
   <div class="input-field">
-    <input type="text" name="legal-responsible-document" id="js-legal-responsible-document">
-    <label>CPF do responsável</label>
+    <input type="text" name="legal-responsible-document" id="js-legal-responsible-document" required>
+    <label>CPF do responsável *</label>
   </div>
 </div>
 
 <div class="input-field">
-  <input type="text" name="email" id="js-patient-email" value="{{ isset($patient->email) ? $patient->email : '' }}">
-  <label>E-mail</label>
+  <input type="text" name="email" id="js-patient-email" required>
+  <label>E-mail *</label>
+</div>
+
+<div class="input-field patients-phone-wrapper">
+  <input type="text" name="phone" class="js-patient-phone" required>
+  <label>Telefone *</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="zip_code" id="js-patient-zip-code" value="{{ isset($patient->zip_code) ? $patient->zip_code : '' }}">
-  <label>CEP</label>
+  <input type="text" name="zip_code" id="js-patient-zip-code" required>
+  <label>CEP *</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="address" id="js-patient-address" value="{{ isset($patient->address) ? $patient->address : '' }}">
-  <label>Endereço</label>
+  <input type="text" name="address" id="js-patient-address" required>
+  <label>Endereço *</label>
 </div>
 
 <div class="input-field">
-  <input type="text" name="address_number" id="js-patient-address-number" value="{{ isset($patient->address_number) ? $patient->address_number : '' }}">
+  <input type="text" name="address_number" id="js-patient-address-number">
   <label>Nº</label>
 </div>
