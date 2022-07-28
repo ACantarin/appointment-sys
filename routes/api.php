a83db8963385fde24fab2222697c7ac28044d2a3
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-27 22:11:07 -0300

    Inclui rota GET de CPF no arquivo de rotas da API

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\AddressController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/address/{zip_code}", [AddressController::class, "show"]);
+Route::get("/patients/document/{document}",["as" => "patients.document", "uses" => "App\Http\Controllers\PatientController@getDocument"]);
