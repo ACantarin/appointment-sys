775c8ea74ab3e58d34f3e695be03c74a32e77707
André Cantarin do Rosário <andrecantarin@gmail.com>
2022-07-28 14:34:14 -0300

    Inclui rota para validação de CRM existente

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/address/{zip_code}", [AddressController::class, "show"]);
 Route::get("/patients/document/{document}",["as" => "patients.document", "uses" => "App\Http\Controllers\PatientController@getDocument"]);
+Route::get("/doctors/register-number/{register_number}",["as" => "doctors.registerNumber", "uses" => "App\Http\Controllers\DoctorController@getRegisterNumber"]);
